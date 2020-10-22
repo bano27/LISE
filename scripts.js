382d794677842f7a3ddfe562d8e89e5a05463a91
erastus <erastuskariuki15@gmail.com>
2020-10-22 08:20:00 +0300

    add js

--- scripts.js
+++ scripts.js
@@ -1,88 +1,63 @@
 function myfunction() {
 
-  function bigImg(x) {
-    x.style.height = "800px";
-    x.style.width = "800px";
-  }
-  
-  function normalImg(x) {
-    x.style.height = "32px";
-    x.style.width = "32px";
-  }
+  //  <...............get get art selected...........>
+
+    let userInput
+    var art1 = document.getElementById("african-woman").Value
+    var art2 = document.getElementById("public-vehicle").Value
+    var art3 = document.getElementById("local-heros").Value
+    var art4 = document.getElementById("beauty-of-nature").Value
+    var art5 = document.getElementById("african-heritage").Value
+    var art6 = document.getElementById("african").Value
+
+    //  <...............get pencil art selected...........>
+
+    var pencil1 = document.getElementById("media-personality").Value
+    var pencil2 = document.getElementById("politician").Value
+    var pencil3 = document.getElementById("comedian").Value
+    var pencil4 = document.getElementById("actors").Value
+    var pencil5 = document.getElementById("animals").Value
+    var pencil6 = document.getElementById("founding-fathers").Value
+
+    //  <...............get sculture selected...........>
+
+    var sculpture1 = document.getElementById("unity").Value
+    var sculpture2 = document.getElementById("zoo").Value
+    var sculpture3 = document.getElementById("zoo-animal").Value
+    var sculpture4 = document.getElementById("owl").Value
+    var sculpture5 = document.getElementById("ostrig").Value
+    var sculpture6 = document.getElementById("elephant").Value
+
+  //  <...............get tattoo selected...........>
+
+    var tattoo1 = document.getElementById("3d").Value
+    var tattoo2 = document.getElementById("simple").Value
+    var tattoo3 = document.getElementById("2d").Value
+    var tattoo4 = document.getElementById("2d-2").Value
+    var tattoo5 = document.getElementById("simple-drawing1").Value
+    var tattoo6 = document.getElementById("simple-drawing2").Value
+
+
+    userInput = art1 + "/" + art2 + "/" + art3 + "/" + art4 + "/" + art5 + "/" + art6 + "/" + pencil1 + "/" + pencil2 + "/" + pencil3 + "/" + pencil4 + "/" + pencil5 + "/" + pencil6 + "/" + sculpture1 + "/" + sculpture2 + "/" + sulpture3 + "/" + sculpture4 + "/" + sculpture5 + "/" + sculpture6 + "/" + tattoo1 + "/" + tattoo2 + "/" + tattoo3 + "/" + tattoo4 + "/" + tattoo5 + "/" + tattoo6
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//     let userInput
-//     var art1 = document.getElementById("african-woman").Value
-//     var art2 = document.getElementById("public-vehicle").Value
-//     var art3 = document.getElementById("local-heros").Value
-//     var art4 = document.getElementById("beauty-of-nature").Value
-//     var art5 = document.getElementById("african-heritage").Value
-//     var art6 = document.getElementById("african").Value
-
-//     var pencil1 = document.getElementById("media-personality").Value
-//     var pencil2 = document.getElementById("politician").Value
-//     var pencil3 = document.getElementById("comedian").Value
-//     var pencil4 = document.getElementById("actors").Value
-//     var pencil5 = document.getElementById("animals").Value
-//     var pencil6 = document.getElementById("founding-fathers").Value
-
-
-//     var sculpture1 = document.getElementById("unity").Value
-//     var sculpture2 = document.getElementById("zoo").Value
-//     var sculpture3 = document.getElementById("zoo-animal").Value
-//     var sculpture4 = document.getElementById("owl").Value
-//     var sculpture5 = document.getElementById("ostrig").Value
-//     var sculpture6 = document.getElementById("elephant").Value
-
-
-//     var tattoo1 = document.getElementById("3d").Value
-//     var tattoo2 = document.getElementById("simple").Value
-//     var tattoo3 = document.getElementById("2d").Value
-//     var tattoo4 = document.getElementById("2d-2").Value
-//     var tattoo5 = document.getElementById("simple-drawing1").Value
-//     var tattoo6 = document.getElementById("simple-drawing2").Value
-
-
-//     userInput = art1 + "/" + art2 + "/" + art3 + "/" + art4 + "/" + art5 + "/" + art6 + "/" + pencil1 + "/" + pencil2 + "/" + pencil3 + "/" + pencil4 + "/" + pencil5 + "/" + pencil6 + "/" + sculpture1 + "/" + sculpture2 + "/" + sulpture3 + "/" + sculpture4 + "/" + sculpture5 + "/" + sculpture6 + "/" + tattoo1 + "/" + tattoo2 + "/" + tattoo3 + "/" + tattoo4 + "/" + tattoo5 + "/" + tattoo6
-
-// }
-// if (price === ".price") {
-//     $("#africa-woman").show();
-//     $("#public-vehicle").show();
-//     $("#local-heroes").show();
-//     $("#beauty-of-nature").show();
-//     $("#african-heritage").show();
-//     $("#public-vehicles").show();
-//     $(".art").append();
-//     totalCost;
-//     $("#total-amount").empty();
-//     $("#total-amount").append(totalCost);
-//   } else {
-//     alert(
-//       totalCost +
-//       ". Your order will be delivered within the next 24hrs!"
-//     );
-    
+if (price === ".price") {
+    $("#africa-woman").show();
+    $("#public-vehicle").show();
+    $("#local-heroes").show();
+    $("#beauty-of-nature").show();
+    $("#african-heritage").show();
+    $("#public-vehicles").show();
+    $(".art").append();
+    totalCost;
+    $("#total-amount").empty();
+    $("#total-amount").append(totalCost);
+  } else {
+    alert(
+      totalCost +
+      ". Your order will be delivered within the next 24hrs!"
+    );
+    }  
 
 
 
